5cf688c3da70cbfadc8f57b22ce76f27c1e93fc6
Felix <tinylambda@gmail.com>
2016-01-14 14:36:43 +0800

    Add container start script

---
+++ container_start.sh
@@ -0,0 +1,10 @@
+docker run -itd \
+--net=host \
+--env-file /root/container_env \
+--entrypoint "/root/config.py" \
+--privileged=true \
+-v /data/docker-vm/tmp/:/tmp/ \
+-v /data/docker-vm/var/log/zookeeper/:/var/log/zookeeper/  \
+-v /data/docker-vm/var/lib/zookeeper:/var/lib/zookeeper/ \
+-v /data/dfs:/data1/dfs \
+tinylambda/hadoop-base:x2
