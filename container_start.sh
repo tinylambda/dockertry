75066467a7f07406a2dbe3752e0cc259e3a4bc9d
Felix <tinylambda@gmail.com>
2016-01-14 14:49:56 +0800

    change entrypoint

--- container_start.sh
+++ container_start.sh
@@ -1,7 +1,7 @@
 docker run -itd \
 --net=host \
 --env-file /root/container_env \
---entrypoint "/root/config.py" \
+--entrypoint "/root/utils/config.py" \
 --privileged=true \
 -v /data/docker-vm/tmp/:/tmp/ \
 -v /data/docker-vm/var/log/zookeeper/:/var/log/zookeeper/  \
